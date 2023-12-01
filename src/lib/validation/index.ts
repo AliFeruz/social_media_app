fa2aa764dd20d49b513707806362905c8181316c
AliFeruz <aliferuzcode@gmail.com>
2023-12-01 15:58:43 +0100

    CreatePost added

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,8 +8,15 @@ export const SignUpValidation = z.object({
 
   });
 
-  export const SignInValidation = z.object({
+export const SignInValidation = z.object({
     email: z.string().email(),
     password: z.string().min(7, { message: 'Password must be at least 7 characters'}),
 
-  });
+});
+
+export const PostValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
